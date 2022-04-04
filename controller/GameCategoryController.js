45b98f30454a64ca3a248964126a4b56767aeed4
ThanhPhong <phongwow201286@gmail.com>
2022-04-04 16:20:28 +0700

    comit 2

--- controller/GameCategoryController.js
+++ controller/GameCategoryController.js
@@ -5,15 +5,19 @@ const GameCategory =require('../models/GameCategory');
 class GamesCategoryController{
     // /api/categoryGame/postGame
     async postGame(req,res){
-        const GameName =req.body.gameName;
+        const GameName =req.body.gameName.trim();
         const GameImage =req.body.gameImage;
-        const FullDetail =req.body.fullDetail;
+        const FullDetail =req.body.fullDetail.trim();
         const ImageDetail =req.body.imageDetail;
 
         try {
             if(!GameName || !GameImage || !FullDetail || !ImageDetail){
-                return res.json({success: false, message: 'Thiếu dữ liệu cần thiết !!'+GameName+"/"+GameImage+"/"+FullDetail+"/"+ImageDetail});
+                return res.json({success: false, message: 'Thiếu dữ liệu cần thiết !!'});
             }else{
+                const findGameName =await GameCategory.findOne({gameName: GameName});
+                if(findGameName){
+                    return res.json({success: false, message: 'Tên game đã tồn tại'})
+                }
                 const LinkGameImage =await upload.UploadFile(GameImage);
                 const LinkImageDetail=await upload.UploadFile(ImageDetail);
                 if(!LinkGameImage=="" && !LinkImageDetail ==""){
@@ -24,7 +28,86 @@ class GamesCategoryController{
             }
         } catch (error) {
             console.log(error);
-            return res.status(500).json({success: false, message: 'Lỗi !!'});
+            return res.json({success: false, message: 'Lỗi !!'});
+        }
+    }
+
+    async getAllGame(req,res){
+        try {
+            const listAllGame =await GameCategory.find();
+            return res.json({success: true,listAllGame});
+        } catch (error) {
+            console.log(error);
+            return res.json({success: false, message: 'Lỗi !!'});
+        }
+    }
+
+    async getGameById(req,res){
+        try {
+            const IdGame =req.body.idGame;
+            if(!IdGame){
+                return res.json({success: false, message: 'Thiếu dữ liệu cần thiết !!'});
+            }
+            const ResultGameById = await GameCategory.findById(IdGame);
+            return res.json({success:true,ResultGameById});
+        } catch (error) {
+            console.log(error);
+            return res.json({success: false, message: 'Lỗi !!'});
+        }
+    }
+
+    async updateGameById(req,res){
+        try {
+            const upIdGame =req.body.idGame;
+            const upGameName =req.body.gameName;
+            const upGameImage =req.body.gameImage;
+            const upFullDetail =req.body.fullDetail;
+            const upImageDetail =req.body.imageDetail;
+            if(!upIdGame){
+                return res.json({success: false, message: 'Thiếu dữ liệu cần thiết !!'});
+            }
+            const upResultGame = await GameCategory.findById(upIdGame);
+            var flag =false;
+            if(upResultGame.GameName != upGameName){
+                await GameCategory.findByIdAndUpdate(upIdGame,{gameName:upGameName.trim()});
+                flag =true;
+            }
+            if(upResultGame.FullDetail != upFullDetail){
+                await GameCategory.findByIdAndUpdate(upIdGame,{fullDetail:upFullDetail.trim()});
+                flag =true;
+            }
+            if(upGameImage && !upGameImage==""){
+                const upLinkGameImage =await upload.UploadFile(upGameImage);
+                await GameCategory.findByIdAndUpdate(upIdGame,{gameImage:upLinkGameImage.trim()});
+                flag =true;
+            }
+            if(upImageDetail && !upImageDetail==""){
+                const upLinkImageDetail =await upload.UploadFile(upImageDetail);
+                await GameCategory.findByIdAndUpdate(upIdGame,{imageDetail:upLinkImageDetail.trim()});
+                flag =true;
+            }
+            if(flag==false){
+                return res.json({success: false, message: "Không có gì cần cập nhật"});
+            }else{
+                return res.json({success: true,message: "Cập nhật thành công"});
+            }
+        } catch (error) {
+            console.log(error);
+            return res.json({success: false, message: 'Lỗi !!'});
+        }
+    }
+
+    async deleteById(req,res){
+        try {
+            const delIdGame =req.body.idGame;
+            if(!delIdGame){
+                return res.json({success: false, message: 'Thiếu dữ liệu cần thiết !!'});
+            }
+            await GameCategory.findByIdAndDelete(delIdGame);
+            return res.json({success: true,message: "Xóa thành công"});
+        } catch (error) {
+            console.log(error);
+            return res.json({success: false, message: 'Lỗi !!'});
         }
     }
 }
--- routers/GameCategory.routes.js
+++ routers/GameCategory.routes.js
@@ -3,5 +3,9 @@ const router =express.Router();
 const GameCategoryController =require('../controller/GameCategoryController');
 
 router.post('/postGame',GameCategoryController.postGame);
+router.get('/getAllGame',GameCategoryController.getAllGame);
+router.get('/getGameById',GameCategoryController.getGameById);
+router.put('/updateGameById',GameCategoryController.updateGameById);
+router.delete('/delGameById',GameCategoryController.deleteById);
 
 module.exports =router;
