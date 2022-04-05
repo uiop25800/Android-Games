515af910b4f512ccdf480627e9ac5bc60c8479a9
ThanhPhong <phongwow201286@gmail.com>
2022-04-05 12:05:12 +0700

    commit2

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app =express();
 
 dotenv.config();
 
-const PORT = process.env.port;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
